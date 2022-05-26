551d1feac93e40acfa688eade5c3675b95b53f2f
hristaki09 <hristaki09@gmail.com>
2022-05-26 11:14:48 +0300

    math div

--- math.py
+++ math.py
@@ -1,12 +1,16 @@
 # print("Math...")
-from ast import For
-
 
 def add_numbers(x, y):
     return x + y
 
+
 def subtr_numbers(x, y):
     return x - y
 
+
 def mult_numbers(x, y):
     return x * y
+
+
+def div_numbers(x, y):
+    return x / y
