6776a11e1d0950b0ccd9d7731c7e31f341494566
hristaki09 <hristaki09@gmail.com>
2022-05-26 10:51:53 +0300

    math subtr

--- math.py
+++ math.py
@@ -1,10 +1,12 @@
 # print("Math...")
+from ast import For
+
+
 def add_numbers(x, y):
     return x + y
 
 def subtr_numbers(x, y):
-    return
+    return x - y
 
 def mult_numbers(x, y):
     return x * y
-
